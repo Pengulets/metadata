7c2ab974948a16e59bfada1decb0b562baa1077d
QuantumlyTangled <ethoarx@gmail.com>
2021-04-13 19:04:19 +0200

    feat: route basics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
+import RoutesBSCPengus from 'routes/bsc/pengus';
 
 const app = fastify();
 void app.register(fastifyCors, {
@@ -7,5 +8,7 @@ void app.register(fastifyCors, {
 	origin: true,
 	credentials: true
 });
+
+void app.register(RoutesBSCPengus, { prefix: '/v1/pengus/' });
 
 app.listen(8080, '0.0.0.0', console.error);
---
+++ src/routes/bsc/pengus/index.ts
@@ -0,0 +1,7 @@
+import { FastifyPluginCallback } from 'fastify';
+
+export const RoutesBSCPengus: FastifyPluginCallback = (_fastify, _, done) => {
+	done();
+};
+
+export default RoutesBSCPengus;
