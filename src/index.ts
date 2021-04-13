4a2ee8a2155af5abcc339977518f4c329a19077e
QuantumlyTangled <ethoarx@gmail.com>
2021-04-13 18:27:14 +0200

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
 
 const app = fastify();
+void app.register(fastifyCors, {
+	maxAge: 600,
+	origin: true,
+	credentials: true
+});
 
 app.listen(8080, '0.0.0.0', console.error);
